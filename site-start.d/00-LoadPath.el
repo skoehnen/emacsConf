ae47580fc904349085557b252c5bbcba1618d140
Stefan Köhnen <s.koehnen@fz-juelich.de>
2015-06-10 13:51:29 +0200

    Added yasnippet

--- site-start.d/00-LoadPath.el
+++ site-start.d/00-LoadPath.el
@@ -1,5 +1,10 @@
 ;; Add external packets
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
+;; nyan-mode
 (add-to-list 'load-path "~/.emacs.d/site-lisp/nyan-mode")
 (require 'nyan-mode )
+
+;; yasnippet
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yasnippet")
+(require 'yasnippet)
---
+++ site-start.d/11-Yasnippet.el
@@ -0,0 +1,2 @@
+;; Set yasnippet as global minor mode
+(yas-global-mode 1)
